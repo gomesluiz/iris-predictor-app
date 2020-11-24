a3dfd313ac0ef2064e5a0b461fcc5ede0a744a1a
Luiz Alberto <gomes.luiz@gmail.com>
2020-11-24 18:42:23 -0300

    feat: Integrate prediction model with web app
    The web app is enabled to read and predict the iris specie of unknown
    data. The predicting model was stored in app/model using python joblib.
    
    Resolves: #1

--- app/main.py
+++ app/main.py
@@ -1,30 +1,37 @@
+import joblib
+import sys
+
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 
-from wtforms import StringField, SubmitField
+from wtforms import FloatField, SubmitField
 from wtforms.validators import DataRequired
 
-import sys
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'wP4xQ8hU1jJ5oI1c'
 bootstrap = Bootstrap(app)
 
-class IrisForm(FlaskForm):
-    sepal_length = StringField('Sepal Length:', validators=[DataRequired()])
-    sepal_width  = StringField('Sepal Width :', validators=[DataRequired()])
-    petal_length = StringField('Petal Length:', validators=[DataRequired()])
-    petal_width  = StringField('Petal Width :', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+class InputForm(FlaskForm):
+    sepal_length = FloatField('Sepal Length:', validators=[DataRequired()])
+    sepal_width  = FloatField('Sepal Width :', validators=[DataRequired()])
+    petal_length = FloatField('Petal Length:', validators=[DataRequired()])
+    petal_width  = FloatField('Petal Width :', validators=[DataRequired()])
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form   = IrisForm(request.form)
+    form   = InputForm(request.form)
     specie = 'No-image'
-    print("Call me", file=sys.stderr)
     if form.validate_on_submit():
-        print("Successfully created a new book", file=sys.stderr)
-        specie = 'Iris-setosa' 
+        x = [[form.sepal_length.data, form.sepal_width.data, 
+              form.petal_length.data, form.petal_width.data]]
+        specie = make_prediction(x) 
+        print(specie)
 
     return render_template('index.html', form=form, specie=specie)
+
+def make_prediction(X):
+    filename = 'model/finalized_model.sav'
+    model = joblib.load(filename)
+    return model.predict(X)[0]
